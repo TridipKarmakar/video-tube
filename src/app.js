76b631bc00df07d8bd8f8b233e1ce4f56015c993
Tridip Karmakar <karmakartridip97@gmail.com>
2025-07-04 23:41:17 +0530

    connect to the mongodb

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // e.g. 'http://localhost:3000'
+    credentials: true, // Allow cookies, sessions, tokens to be sent
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("hello form tridip");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./.env" });
+
+const PORT = process.env.PORT || 8001;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection error ", err);
+  });
